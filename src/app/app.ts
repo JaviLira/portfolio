80ef368adbcab22980ee6268dfcbcb6d21d4aa42
Francisco Javier Lira Sánchez <javilirasanchez20011812@gmail.com>
2026-02-18 23:30:29 +0100

    start home

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBar } from "./components/nav-bar/nav-bar";
+import { Home } from "./components/home/home";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavBar],
+  imports: [RouterOutlet, NavBar, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
