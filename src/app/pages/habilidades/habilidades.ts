c5b5b28271a22caf9aa11b1ff59d6a828e24edae
Francisco Javier Lira Sánchez <javilirasanchez20011812@gmail.com>
2026-02-24 00:30:21 +0100

    grafica habilidades empezada

--- src/app/pages/habilidades/habilidades.ts
+++ src/app/pages/habilidades/habilidades.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+interface Skill {
+  name: string;
+  percent: number;
+  isLight?: boolean;
+}
 @Component({
   selector: 'app-habilidades',
   imports: [],
   templateUrl: './habilidades.html',
   styleUrl: './habilidades.css',
 })
-export class Habilidades {
+export class Habilidades implements OnInit {
+  public animated = false;
+  
+  public skills: Skill[] = [
+    { name: 'Photoshop', percent: 85 },
+    { name: 'Illustrator', percent: 75, isLight: true },
+    { name: 'Indesign', percent: 65 },
+    { name: 'HTML', percent: 90, isLight: true },
+    { name: 'CSS', percent: 90 },
+    { name: 'jQuery', percent: 80, isLight: true },
+    { name: 'RWD', percent: 85, isLight: true },
+    { name: 'PHP', percent: 75 },
+    { name: 'WordPress', percent: 80, isLight: true },
+    { name: 'SASS/SCSS', percent: 70 }
+  ];
 
+  ngOnInit() {
+    // Disparamos la animación tras un breve delay
+    setTimeout(() => this.animated = true, 500);
+  }
 }
