1b023fbb67c4f20fa43dddff29ca73bd8b0492f4
Francisco Javier Lira Sánchez <javilirasanchez20011812@gmail.com>
2026-05-04 23:18:15 +0200

    start formacion

--- src/app/pages/formacion/formacion.ts
+++ src/app/pages/formacion/formacion.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+interface Formacion_i {
+  titulo: string;
+  institucion: string;
+  fecha: string;
+  descripcion: string;
+  tipo: 'grado' | 'certificacion' | 'curso';
+}
 
 @Component({
   selector: 'app-formacion',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './formacion.html',
   styleUrl: './formacion.css',
 })
 export class Formacion {
+  public formaciones: Formacion_i[] = [
+    {
+      titulo: 'Grado en Ingeniería Informática',
+      institucion: 'Universidad Ejemplo',
+      fecha: '2018 - 2022',
+      descripcion: 'Especialización en Ingeniería del Software y Sistemas de Información.',
+      tipo: 'grado'
+    }
+  ];
 
 }
